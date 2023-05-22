3ff5d914003f94e092b653f35984ab84a484275a
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-05-22 09:33:04 +0200

    feat(#48): delete farmland

--- src/components/NewFarmlandScreen/NewFarmlandScreen.js
+++ src/components/NewFarmlandScreen/NewFarmlandScreen.js
@@ -9,6 +9,9 @@ import {
   Autocomplete,
   Snackbar,
   Alert,
+  Dialog,
+  Card,
+  CardContent,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import FullScreenDialog from "../UI/FullScreenDialog/FullScreenDialog";
@@ -22,6 +25,8 @@ import { useEffect } from "react";
 import useCompanies from "../../hooks/useCompanies";
 
 const NewFarmlandScreen = ({ onClose, farmlandId }) => {
+  const [isNewFarmland, setIsNewFarmland] = useState(farmlandId ? false : true);
+  const [isDelFarmland, setIsDelFarmland] = useState(false);
   const [open, setOpen] = useState(true);
   const [area, setArea] = useState();
   const [perimeter, setPerimeter] = useState();
@@ -34,6 +39,10 @@ const NewFarmlandScreen = ({ onClose, farmlandId }) => {
     setOpen(false);
     onClose("farmlands");
   }, [onClose]);
+
+  const handleDelOnClose = useCallback(() => {
+    setIsDelFarmland(false);
+  }, [isDelFarmland]);
 
   const onSaveFarmHandler = useCallback(() => {
     const data = farmlandLoader.getItems();
@@ -49,6 +58,20 @@ const NewFarmlandScreen = ({ onClose, farmlandId }) => {
     farmlandLoader.storeItems([...data, newFarmland]);
     handleOnClose();
   }, [owner, coordinates]);
+
+  const deleteHandler = useCallback(() => {
+    setIsDelFarmland(true);
+  }, []);
+  const confirmDelete = useCallback(() => {
+    setIsDelFarmland(false);
+    setOpen(false);
+    onClose("farmlands");
+
+    const data = farmlandLoader.getItems();
+    const updated = data.filter((farmland) => farmland.id !== farmlandId);
+    farmlandLoader.storeItems(updated);
+    handleOnClose();
+  }, []);
 
   const drawCompletedHandler = useCallback(
     ({ area, perimeter, coordinates }) => {
@@ -195,9 +218,45 @@ const NewFarmlandScreen = ({ onClose, farmlandId }) => {
               )}
             ></Autocomplete>
           </FormControl>
-          <div className={classes.buttons}></div>
+          <div className={classes.buttons}>
+            <Button onClick={deleteHandler}>delete farmland</Button>
+          </div>
         </form>
       </div>
+      {isDelFarmland && (
+        <Dialog
+          open={open}
+          onClose={handleDelOnClose}
+          // TransitionComponent={Transition}
+        >
+          <Card>
+            <AppBar sx={{ position: "relative" }}>
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  onClick={handleDelOnClose}
+                  aria-label="close"
+                >
+                  <CloseIcon />
+                </IconButton>
+                <Typography
+                  sx={{ ml: 2, flex: 1 }}
+                  variant="h6"
+                  component="div"
+                >
+                  Delete farmland
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <CardContent>
+              <h2>Do you want to delete this farmland?</h2>
+
+              <Button onClick={confirmDelete}>delete</Button>
+            </CardContent>
+          </Card>
+        </Dialog>
+      )}
       <Snackbar
         severity="error"
         open={!!error}
