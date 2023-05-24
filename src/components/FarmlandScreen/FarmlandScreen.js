09ff6e5250cb912fe83042697a34cdf127184e6d
Luca Gandini <luca.gandini88@gmail.com>
2023-05-25 00:03:19 +0200

    fix(#6): Close Farmland screen

--- src/components/FarmlandScreen/FarmlandScreen.js
+++ src/components/FarmlandScreen/FarmlandScreen.js
@@ -1,17 +1,32 @@
 import WorldMap from "../WorldMap/WorldMap";
 import classes from "./FarmlandScreen.module.css";
 import farmlandLoader from "../../data/farmlandLoader";
+import FullScreenDialog from "../UI/FullScreenDialog/FullScreenDialog";
 
-const FarmlandScreen = ({ farmlandId }) => {
+import { useState } from "react";
+
+const FarmlandScreen = ({ farmlandId, onClose }) => {
+  const [open, setOpen] = useState(true);
   const data = farmlandLoader.getItems();
   const farm = data.find((farm) => farm.id === farmlandId);
 
+  const handleOnClose = () => {
+    setOpen(false);
+    onClose();
+  };
+
   return (
-    <div className={classes.layoutBody}>
-      <div className={classes.layoutContent}>
-        <WorldMap coordinates={farm.coordinates} />
+    <FullScreenDialog
+      open={open}
+      handleOnClose={handleOnClose}
+      title="Farmland details"
+    >
+      <div className={classes.layoutBody}>
+        <div className={classes.layoutContent}>
+          <WorldMap coordinates={farm.coordinates} />
+        </div>
       </div>
-    </div>
+    </FullScreenDialog>
   );
 };
 export default FarmlandScreen;
